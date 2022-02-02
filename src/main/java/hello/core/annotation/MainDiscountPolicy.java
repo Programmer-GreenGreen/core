fc07313f2ad7cd566472d604430f378d161f7970
Green <fhrrud0334@gamil.com>
2022-02-02 15:49:23 +0900

    MarkAnnotation의 문제

--- src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.lang.annotation.*;
 
+
+//markAnnotation 은 Runtime , 예외가 터지면 복잡해짐
+//Runtime 을 줄이고 컴파일에서 예외를 잡을 수 없을까? => markAnnotation 대신에 MarkInterface
+//MarkInterface Body가 없고 MarkInterface를 상속받은 클래스만 사용할 수 있다. (~~~able) => MarkInterface를
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
@@ -12,6 +14,7 @@ public class LogDemoService {
     private final MyLogger myLogger;
 
     public void logic (String id){
+        Objects.requireNonNull(id);
 
         myLogger.log("service id " + id);
     }
